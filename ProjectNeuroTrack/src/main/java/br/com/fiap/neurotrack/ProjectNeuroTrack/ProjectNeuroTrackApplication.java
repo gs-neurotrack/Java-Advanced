4c6ec38e2b8dc99812840caa0d76fddbc167b1e5
devpedrosena1 <devpedrosena1@gmail.com>
2025-11-12 14:31:50 -0300

    creating project at global solution

---
+++ ProjectNeuroTrack/src/main/java/br/com/fiap/neurotrack/ProjectNeuroTrack/ProjectNeuroTrackApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.neurotrack.ProjectNeuroTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNeuroTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNeuroTrackApplication.class, args);
+	}
+
+}
---
+++ ProjectNeuroTrack/src/test/java/br/com/fiap/neurotrack/ProjectNeuroTrack/ProjectNeuroTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.neurotrack.ProjectNeuroTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNeuroTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
